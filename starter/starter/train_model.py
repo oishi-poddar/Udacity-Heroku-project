14e8571f0db4147ad03addfcf34bb2bfc71fd261
oishi-poddar <oishi.poddar@gmail.com>
2021-06-07 19:09:28 +0530

    change screenshots

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -51,36 +51,6 @@ def train():
     preds = inference(model, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-    # seperate function for model performacne output on a set of categorical features
-    def silice_test_performance(cat_features):
-        print("read pandas")
-        data = pd.read_csv('starter_main/data/census_clean.csv')
-        print("df created: " + str(data.shape))
-
-        # add target variable
-
-        cat_features_list = np.append(cat_features, "salary")
-
-        # filter data
-        data = data[cat_features_list]
-
-        train, test = train_test_split(data, test_size=0.20)
-        X_train, y_train, encoder, lb = mldata.process_data(
-            train, categorical_features=cat_features, label="salary", training=True
-        )
-
-        # Proces the test data with the process_data function.
-        print("start process test datafunction")
-        X_test, y_test, encoder, lb = mldata.process_data(
-            test, categorical_features=cat_features, label="salary", training=False,
-            encoder=encoder, lb=lb
-        )
-        # Train and save a model.
-        model = mlmodel.train_model(X_train, y_train)
-        preds_test = mlmodel.inference(model, X_test)
-        # calc score
-        precision, recall, fbeta = mlmodel.compute_model_metrics(y_test, preds_test)
-        return precision, recall, fbeta
     with open("slice_outpt.txt", "w") as file:
         file.write("Precision " + str(precision) +'\n')
         file.write("Recall " + str(recall) + '\n')
