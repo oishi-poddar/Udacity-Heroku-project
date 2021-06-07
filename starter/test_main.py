e00c82c28bb070f923781db6b26ea2cd028e2e4c
oishi-poddar <oishi.poddar@gmail.com>
2021-06-07 19:51:43 +0530

    change tests

--- starter/test_main.py
+++ starter/test_main.py
@@ -8,50 +8,50 @@ def test_app_get():
     assert isinstance(response.content,bytes)
     assert str(response.content == "Welcome to my app")
 
-def test_app_prediction():
-    dict={
-      "workclass": "Self-emp-not-inc",
+def test_app_prediction_50k_below():
+    dictionary = {
+      "age": 39,
+      "fnlgt": 77516,
+      "education-num": 13,
+      "capital-gain": 2174,
+      "capital-loss": 0,
+      "hours-per-week": 40,
+      "workclass": "State-gov",
       "education": "Bachelors",
-      "marital_status": "Married-civ-spouse",
-      "occupation": "Exec-managerial",
-      "relationship": "Husband",
+      "marital-status": "Never-married",
+      "occupation": "Adm-clerical",
+      "relationship": "Not-in-family",
       "race": "White",
       "sex": "Male",
-      "native_country": "United-States",
-      "age": "52",
-      "fnlgt": "209642",
-      "education_num":"9",
-      "capital_gain" : "0",
-      "capital_loss": "0",
-      "hours_per_week":"45"
-        }
+      "native-country": "United-States"
+    }
     client = TestClient(app)
-    response = client.post("/model/", json=dict)
+    response = client.post("/model/", json=dictionary)
     assert response.status_code == 200
     assert isinstance(response.content, bytes)
     # assert list(response.json()) == [1]  #  testing one possible case where resturn value is 1
-    assert response.text == '\">50K\"'
+    assert response.text == '\"<=50K\"'
 
-def test_app__another_prediction():
-    dict={
-        "workclass": "Private",
-      "education": "9th",
-      "marital_status": "Married-spouse-absent",
-      "occupation": "Other-service",
-      "relationship": "Not-in-family",
-      "race": "Black",
-      "sex": "Female",
-      "native_country": "Cuba",
-      "age": 49,
-      "fnlgt": 160187,
-      "education_num": 5,
-      "capital_gain": 0,
-      "capital_loss": 0,
-      "hours_per_week": 16
-        }
+def test_app__another_prediction_above_50k():
+    dictionary={
+        "age": 52,
+        "fnlgt": 209642,
+        "education-num": 9,
+        "capital-gain": 123387,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "workclass": "Self-emp-not-inc",
+        "education": "Bachelors",
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "native-country": "United-States"
+    }
     client = TestClient(app)
-    response = client.post("/model/", json=dict)
+    response = client.post("/model/", json=dictionary)
     assert response.status_code == 200
     assert isinstance(response.content, bytes)
     # assert list(response.json()) == [0]   #  testing another possible case where return value is 0
-    assert response.text == '\"<=50K\"'
+    assert response.text == '\">50K\"'
